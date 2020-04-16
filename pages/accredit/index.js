d4f533ff39ad59e3ab620e7ba4e5992d6f890fca
xzt246679 <51944081+xzt246679@users.noreply.github.com>
2020-04-16 11:26:23 +0800

    1
    ada

--- pages/accredit/index.js
+++ pages/accredit/index.js
@@ -11,7 +11,8 @@ Page({
     //判断小程序的API，回调，参数，组件等是否在当前版本可用。
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     isHide: false,
-    wxcode: ""
+    wxcode: "",
+    c:0
   },
   onShow: function () {
   },
